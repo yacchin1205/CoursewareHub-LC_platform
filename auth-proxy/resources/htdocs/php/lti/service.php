12e4093aa60405128c35f9e73a6a8c5125494fc1
yacchin1205 <yazawa@yzwlab.net>
2020-09-25 15:24:51 +0900

    Fix session

--- auth-proxy/resources/htdocs/php/lti/service.php
+++ auth-proxy/resources/htdocs/php/lti/service.php
@@ -10,7 +10,11 @@ $launch = LTI\LTI_Message_Launch::new(new CoursewareHub_Database())
 
 $mail_address = $launch->get_launch_data()['email'];
 
+session_regenerate_id(true);
+// Set Session info
 $username = get_username_from_mail_address($mail_address);
+$_SESSION['username'] = $username;
+
 header("X-Accel-Redirect: /entrance/");
 header("X-Reproxy-URL: ".HUB_URL.'/'.COURSE_NAME."/hub/login");
 header("X-REMOTE-USER: $username");
